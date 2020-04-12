cda7231e62e0c655a43ab5c5dd07bddaab526657
JohanWennerbeck <jojjanw@gmail.com>
2020-04-12 21:19:09 +0200

    Add checkbox when player is selected

--- app/src/main/java/jaw/cardgame/SelectPlayersFragment.java
+++ app/src/main/java/jaw/cardgame/SelectPlayersFragment.java
@@ -1,6 +1,8 @@
 package jaw.cardgame;
 
+import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 import android.print.PrintAttributes;
 import android.support.v4.app.Fragment;
@@ -9,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
@@ -25,7 +28,8 @@ import jaw.cardgame.util.StorageUtil;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SelectPlayersFragment extends Fragment {
+public class SelectPlayersFragment extends Fragment implements View.OnClickListener{
+    ArrayList<String> chosenPlayers;
 
     public SelectPlayersFragment() {
         // Required empty public constructor
@@ -36,6 +40,7 @@ public class SelectPlayersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        chosenPlayers = new ArrayList<>();
         View v = inflater.inflate(R.layout.fragment_select_players, container, false);
         initButtons(v);
 
@@ -68,18 +73,35 @@ public class SelectPlayersFragment extends Fragment {
                 Button playerButton = new Button(v.getContext());
                 playerButton.setText(playerName);
                 playerButton.setId(100 + i);
-                RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-                lp.setMargins(100, 0, 100, 0);
+                RelativeLayout.LayoutParams lpButton = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                lpButton.setMargins(20, 0, 20, 0);
+                lpButton.addRule(RelativeLayout.CENTER_HORIZONTAL);
                 if(i > 0){
-                    lp.addRule(RelativeLayout.BELOW, allPlayerButtons.get(i-1).getId());
+                    lpButton.addRule(RelativeLayout.BELOW, allPlayerButtons.get(i-1).getId());
                 } else {
-                    lp.addRule(RelativeLayout.BELOW, R.id.select_player_textview);
+                    lpButton.addRule(RelativeLayout.BELOW, R.id.select_player_textview);
                 }
-                ll.addView(playerButton, lp);
 
+                ImageView selectedCheckImage = new ImageView(v.getContext());
+                RelativeLayout.LayoutParams lpImage = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                selectedCheckImage.setImageResource(android.R.color.transparent);
+                selectedCheckImage.setId(200 + i);
+                lpImage.addRule(RelativeLayout.ALIGN_TOP, playerButton.getId());
+                lpImage.addRule(RelativeLayout.ALIGN_BOTTOM, playerButton.getId());
+                lpImage.addRule(RelativeLayout.END_OF, playerButton.getId());
+                playerButton.setOnClickListener((View view) -> {
+                    selectedCheckImage.setImageResource(R.drawable.ic_check_black_24dp);
+                });
+
+                playerButton.setOnLongClickListener((View view2) -> {
+                    selectedCheckImage.setImageResource(android.R.color.transparent);
+                    return true;
+                });
+                
+                ll.addView(selectedCheckImage, lpImage);
+                ll.addView(playerButton, lpButton);
                 allPlayerButtons.add(playerButton);
             }
-
         }
 
         Button myButton = new Button(v.getContext());
@@ -92,10 +114,14 @@ public class SelectPlayersFragment extends Fragment {
         ll.addView(myButton, lp);
         if(allNames.size() < 3 || notEnoughPlayers){
             notEnoughPlayers();
-            return;
         }
     }
 
     private void notEnoughPlayers() {
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
